4314c9e4c2bbcce26c0a2971d3a09a7b7b8efcb9
T-Tenma <1014203967@qq.com>
2016-07-10 20:08:56 +0800

    测试版本控制的提交

--- app/src/main/java/com/hello/tenma/hellostudio/MainActivity.java
+++ app/src/main/java/com/hello/tenma/hellostudio/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //为了确认版本控制是否有效
         setContentView(R.layout.activity_main);
     }
 }
